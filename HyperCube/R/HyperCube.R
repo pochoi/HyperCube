9dda8f8ea56514350a57eb498edec2a7356b5601
CP Choi <choi.chi.po@gmail.com>
2015-05-08 15:27:26 -0700

    Initial package

---
+++ HyperCube/R/HyperCube.R
@@ -0,0 +1,11 @@
+#' Hello world
+#' 
+#' @param name characters string
+#' @examples
+#' ## Say hello!
+#' hello("Amy")
+#' @export
+hello <-
+function(name) {
+  print(paste0("Hello ", name, "!"))
+}
