4f04bd3ca8443f041d139050416c3ca1cddfafba
CP Choi <choi.chi.po@gmail.com>
2015-05-09 22:42:34 -0700

    clean up some git message

--- HyperCube/R/HyperCube.R
+++ HyperCube/R/HyperCube.R
@@ -10,7 +10,6 @@ function(name) {
   print(paste0("Hello ", name, "!"))
 }
 
-<<<<<<< HEAD
 #' Bye world
 #' 
 #' @param name characters string
@@ -22,5 +21,3 @@ bye <-
 function(name) {
   print(paste0("Good Bye ", name, "~"))
 }
-=======
->>>>>>> aa1ca6839a7e58f871984d7c0b528eb9f96f8a98
