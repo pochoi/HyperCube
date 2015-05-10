552d96ee85229c6aa877b3decdad2d197efc7052
Amy Kim <taeyen.kim@gmail.com>
2015-05-09 22:06:35 -0700

    add bye function

--- HyperCube/R/HyperCube.R
+++ HyperCube/R/HyperCube.R
@@ -9,3 +9,15 @@ hello <-
 function(name) {
   print(paste0("Hello ", name, "!"))
 }
+
+#' Bye world
+#' 
+#' @param name characters string
+#' @examples
+#' ## Say Bye!
+#' bye("Po")
+#' @export
+bye <-
+function(name) {
+  print(paste0("Good Bye ", name, "~"))
+}
